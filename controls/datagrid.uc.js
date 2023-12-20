7afb8c71e043f5ab44f86bd29cd35c5bee895c31
pratik2201 <pratik2201.pp@gmail.com>
2023-12-21 02:05:41 +0530

    c

--- controls/datagrid.uc.js
+++ controls/datagrid.uc.js
@@ -1,127 +1,146 @@
-const { resizeManage } = require('@uccontrols:/controls/datagrid.uc.resizeManage.js');
-const { designer } = require('./datagrid.uc.designer.js');
-const { pagerLV } = require('@ucbuilder:/global/listUI/pagerLV');
-const { TemplateNode } = require('@ucbuilder:/Template');
-const { intenseGenerator } = require('@ucbuilder:/intenseGenerator.js');
-const { newObjectOpt } = require('@ucbuilder:/global/objectOpt.js');
-const { simpleScroll } = require('@ucbuilder:/global/listUI/pager/scrollNodes/simpleScroll');
-const { newPagerScroll } = require('@ucbuilder:/global/listUI/pager/scrollNodes/newPagerScroll');
-/** 
+const { resizeManage } = require("@ucbuilder:/global/resizer/resizeManage");
+const { designer } = require("./datagrid.uc.designer.js");
+const { pagerLV } = require("@ucbuilder:/global/listUI/pagerLV");
+const { TemplateNode } = require("@ucbuilder:/Template");
+const { intenseGenerator } = require("@ucbuilder:/intenseGenerator.js");
+const { newObjectOpt } = require("@ucbuilder:/global/objectOpt.js");
+const {
+  simpleScroll,
+} = require("@ucbuilder:/global/listUI/pager/scrollNodes/simpleScroll");
+const {
+  newPagerScroll,
+} = require("@ucbuilder:/global/listUI/pager/scrollNodes/newPagerScroll");
+/**
  * @typedef {import ("@ucbuilder:/global/listUI/pager/scrollNodes/pagerScroll").pagerScroll} pagerScroll
  * @typedef {import ("@ucbuilder:/global/listUI/pager/scrollNodes/simpleScroll").simpleScroll} simpleScroll
  */
 
 class datagrid extends designer {
-    constructor() {
-        eval(designer.giveMeHug);
-        this.init();
-        let cbox = this.detail.scroller.scrollBox;
+  constructor() {
+    eval(designer.giveMeHug);
+    this.init();
+    let cbox = this.detail.scroller.scrollBox;
 
-        this.detail.init(this.detailGridHT1, this.pagercntnr1, this);
+    this.detail.init(this.detailGridHT1, this.pagercntnr1, this);
 
-        let hscroller = new simpleScroll('h');
-        hscroller.init(this.detail, this.hscrollbar1);
-        hscroller.Event.onScroll.on((e) => {
-            this.headerSectionHT.scrollLeft =
-                this.footerSectionHT.scrollLeft = this.hscrollbar1.scrollLeft;
-        });
-        this.pagercntnr1.addEventListener("scroll", (e) => {
-            console.log('s');
-            this.headerSectionHT.scrollLeft =
-                this.footerSectionHT.scrollLeft =
-                this.pagercntnr1.scrollLeft = this.hscrollbar1.scrollLeft;
-        });
-        /*this.colsResizeMng.Events.onResizing.on((evt, diff) => {
+    let hscroller = new simpleScroll("h");
+    hscroller.init(this.detail, this.hscrollbar1);
+    hscroller.Event.onScroll.on((e) => {
+      this.headerSectionHT.scrollLeft = this.footerSectionHT.scrollLeft =
+        this.hscrollbar1.scrollLeft;
+    });
+    this.pagercntnr1.addEventListener("scroll", (e) => {
+      //console.log('s');
+      this.headerSectionHT.scrollLeft =
+        this.footerSectionHT.scrollLeft =
+        this.pagercntnr1.scrollLeft =
+          this.hscrollbar1.scrollLeft;
+    });
+    /*this.colsResizeMng.Events.onResizing.on((evt, diff) => {
             this.pagercntnr1.scrollLeft = this.hscrollbar1.scrollLeft;
         });*/
-        let vscroller = new newPagerScroll('v');
-        vscroller.elementNode.beginText = this.begin_scroll_text;
-        vscroller.elementNode.endText = this.end_scroll_text;
-        vscroller.init(this.detail, this.vscrollbar1);
-        vscroller.Events.onChangeHiddenCount.on((b, e) => {
-            this.begin_scroll_text.innerText = b == 0 ? "" : "▲ " + b + "";
-            this.end_scroll_text.innerText = e == 0 ? "" : "▼ " + e + "";
-        });
+    let vscroller = new newPagerScroll("v");
+    vscroller.elementNode.beginText = this.begin_scroll_text;
+    vscroller.elementNode.endText = this.end_scroll_text;
+    vscroller.init(this.detail, this.vscrollbar1);
+    vscroller.Events.onChangeHiddenCount.on((b, e) => {
+      this.begin_scroll_text.innerText = b == 0 ? "" : "▲ " + b + "";
+      this.end_scroll_text.innerText = e == 0 ? "" : "▼ " + e + "";
+    });
+  }
 
+  _paging = true;
+  get paging() {
+    return this._paging;
+  }
+  set paging(value) {
+    this._paging = value;
+  }
 
+  get varName() {
+    return this.resizer.varName;
+  }
+  set varName(value) {
+    this.resizer.varName = value;
+  }
+  get varValue() {
+    return this.resizer.varValue;
+  }
+  set varValue(value) {
+    this.resizer.varValue = value;
+  }
 
+  get itemTemplate() {
+    return this.detail.itemTemplate;
+  }
+  set itemTemplate(value) {
+    this.detail.itemTemplate = intenseGenerator.parseTPT(
+      value,
+      this.ucExtends.PARENT
+    );
+  }
 
+  /** @type {TemplateNode}  */
+  _headerTemplate = undefined;
+  get headerTemplate() {
+    return this._headerTemplate;
+  }
+  set headerTemplate(value) {
+    this._headerTemplate = intenseGenerator.parseTPT(
+      value,
+      this.ucExtends.PARENT
+    );
+  }
+  /** @type {TemplateNode}  */
+  _footerTemplate = undefined;
+  get footerTemplate() {
+    return this._footerTemplate;
+  }
+  set footerTemplate(value) {
+    this._footerTemplate = intenseGenerator.parseTPT(
+      value,
+      this.ucExtends.PARENT
+    );
+  }
+
+  static dgvFillArgs = {
+    addHeader: true,
+    headerRow: {},
+    fillDetail: true,
+    addFooter: false,
+    footerRow: {},
+  };
+  /** @param {datagrid.dgvFillArgs} params */
+  fill(params) {
+    let args = newObjectOpt.copyProps(params, datagrid.dgvFillArgs);
+    if (args.fillDetail) this.detail.nodes.fill();
+    if (args.addHeader) {
+      this.headerGridHT1.innerHTML = "";
+      this.headerGridHT1.appendChild(
+        this.headerTemplate.extended.generateNode(args.headerRow)
+      );
     }
+    if (args.addFooter) {
+      this.footerGridHT1.innerHTML = "";
+      this.footerGridHT1.appendChild(
+        this.footerTemplate.extended.generateNode(args.footerRow)
+      );
+    }
+  }
 
+  detail = new pagerLV();
 
-    _paging = true;
-    get paging() {
-        return this._paging;
-    }
-    set paging(value) {
-        this._paging = value;
-    }
+  resizer = new resizeManage();
 
-    get varName() {
-        return this.resizer.varName;
-    }
-    set varName(value) {
-        this.resizer.varName = value;
-    }
-    get varValue() {
-        return this.resizer.varValue;
-    }
-    set varValue(value) {
-        this.resizer.varValue = value;
-    }
-
-    get itemTemplate() { return this.detail.itemTemplate; }
-    set itemTemplate(value) {
-        this.detail.itemTemplate = intenseGenerator.parseTPT(value, this.ucExtends.PARENT);        
-    }
-
-    /** @type {TemplateNode}  */
-    _headerTemplate = undefined;
-    get headerTemplate() { return this._headerTemplate; }
-    set headerTemplate(value) {
-        this._headerTemplate = intenseGenerator.parseTPT(value, this.ucExtends.PARENT);
-    }
-     /** @type {TemplateNode}  */
-     _footerTemplate = undefined;
-     get footerTemplate() { return this._footerTemplate; }
-     set footerTemplate(value) {
-         this._footerTemplate = intenseGenerator.parseTPT(value, this.ucExtends.PARENT);;
-     }
-
-    static dgvFillArgs = {
-        addHeader: true,
-        headerRow: {},
-        fillDetail: true,
-        addFooter: false,
-        footerRow: {},
+  init() {
+    let changed = false;
+    this.resizer.options = {
+      container: this.ucExtends.self,
+      grid: this.pagercntnr1,
+      getVarValue: (varname) => this.detail.itemTemplate.extended.getCSS_localVar(varname),
+      setVarValue: (varname,val) => this.detail.itemTemplate.extended.setCSS_localVar( varname, val),
     };
-    /** @param {datagrid.dgvFillArgs} params */
-    fill(params) {
-        let args = newObjectOpt.copyProps(params, datagrid.dgvFillArgs);
-        if (args.fillDetail)
-            this.detail.nodes.fill();
-        if (args.addHeader) {
-            this.headerGridHT1.innerHTML = '';
-            this.headerGridHT1.appendChild(this.headerTemplate.extended.generateNode(args.headerRow));
-        }
-        if (args.addFooter) {
-            this.footerGridHT1.innerHTML = '';
-            this.footerGridHT1.appendChild(this.footerTemplate.extended.generateNode(args.footerRow));
-        }
-    }
-   
-
-    detail = new pagerLV();
-    
-
-    resizer = new resizeManage();
-
-    init() {
-        let changed = false;
-        this.resizer.init(this);
-        
-    }
-
-
+    this.resizer.init();
+  }
 }
 module.exports = datagrid;
--- controls/datagrid.uc.resizeManage.js
+++ controls/datagrid.uc.resizeManage.js
@@ -1,227 +1,242 @@
-const { Rect } = require("@uccontrols:/../ucbuilder/global/drawing/shapes");
-const datagrid = require("@uccontrols:/controls/datagrid.uc");
 const { mouseForMove } = require("@ucbuilder:/global/mouseForMove");
-const { gridResizer } = require("@uccontrols:/../ucbuilder/global/gridResizer");
-const { Point } = require("ucbuilder/global/drawing/shapes");
-const { timeoutCall } = require("ucbuilder/global/timeoutCall");
-const { commonEvent } = require("ucbuilder/global/commonEvent");
+const { gridResizer } = require("@ucbuilder:/global/gridResizer");
+const { Point } = require("@ucbuilder:/global/drawing/shapes");
+const { commonEvent } = require("@ucbuilder:/global/commonEvent");
 class measurementNode {
-    /** @type {number}  */
-    size = 0;
+  /** @type {number}  */
+  size = 0;
 
-    /** @type {number}  */
-    runningSize = 0;
+  /** @type {number}  */
+  runningSize = 0;
 
-    get prevRunningSize() { return this.runningSize - this.size; }
+  get prevRunningSize() {
+    return this.runningSize - this.size;
+  }
 
-    get minVal() { return this.runningSize - 2; }
-    get maxVal() { return this.runningSize + 2; }
+  get minVal() {
+    return this.runningSize - 2;
+  }
+  get maxVal() {
+    return this.runningSize + 2;
+  }
 
-    hasCollission(val) {
-        return val >= this.minVal && val <= this.maxVal;
-    }
+  hasCollission(val) {
+    return val >= this.minVal && val <= this.maxVal;
+  }
 }
 
 class resizeManage {
-    constructor() { }
+  constructor() {}
 
-
-    updateAr() {
-        let counter = 0;
-        this.measurement.forEach((row) => {
-            counter += row.size;
-            row.runningSize = counter;
-        });
-        /*counter = 0;
+  updateAr() {
+    let counter = 0;
+    this.measurement.forEach((row) => {
+      counter += row.size;
+      row.runningSize = counter;
+    });
+    /*counter = 0;
         this.measurementExp.forEach((row) => {
             counter += row.size;
             row.runningSize = counter;
         });*/
+  }
+  hasCollission(val) {
+    let rtrn = { hasCollied: false, index: -1 };
+    for (let i = 0; i < this.measurement.length; i++) {
+      if (this.measurement[i].hasCollission(val)) {
+        rtrn.hasCollied = true;
+        rtrn.index = i;
+        break;
+      }
     }
-    hasCollission(val) {
-        let rtrn = { hasCollied: false, index: -1, };
-        for (let i = 0; i < this.measurement.length; i++) {
-            if (this.measurement[i].hasCollission(val)) { rtrn.hasCollied = true; rtrn.index = i; break; }
-        }
-        return rtrn;
-    }
-    /**
-     * @param {string} txt 
-     * @returns {measurementNode[]}
-     */
-    fillArrFromText(txt) {
-        let ar = txt
-            .split(/ +/);
-        let nlen = ar.length;
-        if (this.measurement.length != nlen) {
-            let rtrn = undefined;
-            this.measurement.length = 0;
+    return rtrn;
+  }
+  /**
+   * @param {string} txt
+   * @returns {measurementNode[]}
+   */
+  fillArrFromText(txt) {
+    let ar = txt.split(/ +/);
+    let nlen = ar.length;
+    if (this.measurement.length != nlen) {
+      let rtrn = undefined;
+      this.measurement.length = 0;
 
-            ar.forEach((s, index) => {
-                if (s === 'auto') {
-                    if (index == nlen - 1) {   // if last element auto
-                        s = Number.MAX_VALUE;
-                    }
-                }
-                let sz = parseFloat(s);
-                rtrn = new measurementNode();
-                rtrn.size = sz;
-                this.measurement.push(rtrn);
-            });
-            this.updateAr();
+      ar.forEach((s, index) => {
+        if (s === "auto") {
+          if (index == nlen - 1) {
+            // if last element auto
+            s = Number.MAX_VALUE;
+          }
+        }
+        let sz = parseFloat(s);
+        rtrn = new measurementNode();
+        rtrn.size = sz;
+        this.measurement.push(rtrn);
+      });
+      this.updateAr();
+    } else {
+      let sz = 0;
+      for (let i = 0; i < ar.length; i++) {
+        sz = parseFloat(ar[i]);
+        if (!isNaN(sz)) this.measurement[i].size = sz;
+      }
+    }
+    console.log(this.measurement.map((s) => s.size).join("px ") + "px");
+  }
+  nameList = gridResizer.getConvertedNames("grid-template-columns");
+
+  /** @type {measurementNode[]}  */
+  measurement = [];
+
+  fillSize = false;
+  options = {
+    /** @type {HTMLElement}  */
+    grid: undefined,
+    /** @type {HTMLElement}  */
+    container: undefined,
+    getVarValue: () => {
+      return "";
+    },
+    setVarValue: (val) => {},
+  };
+  init() {
+    let mouseMv = new mouseForMove();
+    let leftIndex = 0,
+      rightIndex = 0;
+
+    /** @type {measurementNode}  */
+    let rightNode, /** @type {measurementNode}  */ leftNode;
+    /** @type {measurementNode}  */
+    let rightNodeSize, /** @type {measurementNode}  */ leftNodeSize;
+
+    this.isResizing = false;
+    let isSettingSize = false;
+    mouseMv.bind(this.options.container, {
+      onDown: (evt, dpoint) => {
+        this.fillArrFromText(this.options.getVarValue());
+        if (this.collissionResult.hasCollied) {
+          this.updateOffset();
+          leftIndex = this.collissionResult.index;
+          rightIndex = leftIndex + 1;
+          rightNode = this.measurement[rightIndex];
+          leftNode = this.measurement[leftIndex];
+          if (rightNode != undefined) rightNodeSize = rightNode.size;
+          leftNodeSize = leftNode.size;
+
+          this.isResizing = true;
+        } else return false;
+        evt.preventDefault();
+      },
+      onMove: (e, diff) => {
+        let dval = diff.x;
+        if (e.shiftKey || this.fillSize) {
+          //  FILL MODE
+          if (rightNode == undefined) {
+            leftNode.size = leftNodeSize + dval;
+          } else {
+            diff.x =
+              dval > 0
+                ? Math.min(dval, rightNodeSize)
+                : Math.max(dval, leftNodeSize * -1);
+            dval = diff.x;
+            leftNode.size = leftNodeSize + dval;
+            rightNode.size = rightNodeSize - dval;
+          }
         } else {
-            let sz = 0;
-            for (let i = 0; i < ar.length; i++) {
-                sz = parseFloat(ar[i]);
-                if (!isNaN(sz)) this.measurement[i].size = sz;
-            }
+          //  INCREASE MODE
+          leftNode.size = leftNodeSize + dval;
         }
-        console.log(this.measurement
-            .map(s => s.size)
-            .join('px ') + 'px');
-    }
-    nameList = gridResizer.getConvertedNames('grid-template-columns');
-
-    /** @type {measurementNode[]}  */
-    measurement = [];
-
-    fillSize = false;
-    /**
-     * @param {datagrid} main 
-     */
-    init(main) {
-        this.main = main;
-
-        let mouseMv = new mouseForMove();
-        let leftIndex = 0, rightIndex = 0;
-
-        /** @type {measurementNode}  */
-        let rightNode,/** @type {measurementNode}  */ leftNode;
-        /** @type {measurementNode}  */
-        let rightNodeSize,/** @type {measurementNode}  */ leftNodeSize;
-
-        this.isResizing = false;
-        let isSettingSize = false;
-        mouseMv.bind(this.main.ucExtends.self, {
-            onDown: (evt, dpoint) => {
-                this.fillArrFromText(this.varValue);
-                if (this.collissionResult.hasCollied) {
-                    this.updateOffset();
-                    leftIndex = this.collissionResult.index;
-                    rightIndex = leftIndex + 1;
-                    rightNode = this.measurement[rightIndex];
-                    leftNode = this.measurement[leftIndex];
-                    if (rightNode != undefined)
-                        rightNodeSize = rightNode.size;
-                    leftNodeSize = leftNode.size;
-
-                    this.isResizing = true;
-                } else return false;
-                evt.preventDefault();
-            },
-            onMove: (e, diff) => {
-                let dval = diff.x;
-                if (e.shiftKey || this.fillSize) { //  FILL MODE
-                    if (rightNode == undefined) {
-                        leftNode.size = leftNodeSize + dval;
-                    } else {
-                        diff.x = (dval > 0) ? Math.min(dval, rightNodeSize) : Math.max(dval, (leftNodeSize * -1));
-                        dval = diff.x;
-                        leftNode.size = leftNodeSize + dval;
-                        rightNode.size = rightNodeSize - dval;
-                    }
-                } else {   //  INCREASE MODE
-                    leftNode.size = leftNodeSize + dval;
-                }
-                if (isSettingSize) return;
-                isSettingSize = true;
-                setTimeout(() => {
-                    this.varValue = this.measureText;
-                    this.Events.onResizing.fire(e, diff);
-                    isSettingSize = false;
-                }, 1);
-            },
-            onUp: (e, diff) => {
-                if (this.isResizing)
-                    this.updateAr();
-                isSettingSize = false;
-                this.collissionResult.hasCollied = false;
-                this.collissionResult.index = -1;
-                this.isResizing = false;
-            }
-        });
-
-
-
-
-
-        this.main.pagercntnr1.addEventListener("mouseenter", (e) => {
-            this.updateOffset();
-            this.main.ucExtends.self.addEventListener("mousemove", this.mousemovelistner);
-            this.hasMouseEntered = true;
-        });
-        this.main.pagercntnr1.addEventListener("mouseleave", (e) => {
-            this.hasMouseEntered = false;
-            this.main.ucExtends.self.removeEventListener("mousemove", this.mousemovelistner);
-        });
-    }
-    Events = {
-        /**
-         * @type {{on:(callback = (
-         *          evt:MouseEvent,
-         *          diff:Point,
-         * ) =>{})} & commonEvent}
-         */
-        onResizing: new commonEvent(),
-    }
-    parentOffset = new DOMRect();
-    updateOffset() {
-        this.parentOffset = this.main.pagercntnr1.getClientRects()[0];
-        this.parentOffset.x -= this.main.pagercntnr1.scrollLeft;
-        this.parentOffset.y -= this.main.pagercntnr1.scrollTop;
-    }
-    hasMouseEntered = false;
-    isCheckingHoverCollission = false;
-    collissionResult = { hasCollied: false, index: -1 };
-    /** @param {MouseEvent} __e  */
-    mousemovelistner = (__e) => {
-        if (this.isResizing || this.isCheckingHoverCollission) return;
-        let ete = __e;
-        this.isCheckingHoverCollission = true;
+        if (isSettingSize) return;
+        isSettingSize = true;
         setTimeout(() => {
-            if (!this.hasMouseEntered) return;
-            this.isCheckingHoverCollission = false;
-            let x = ete.clientX - this.parentOffset.x;
-            this.collissionResult = this.main.resizer.hasCollission(x);
-            this.main.ucExtends.self.style.cursor = (this.collissionResult.hasCollied) ? 'e-resize' : '';
+          this.options.setVarValue(this.measureText);
+          this.Events.onResizing.fire(e, diff);
+          isSettingSize = false;
         }, 1);
-    }
-    _varName = "gridsize";
-    get varName() { return this._varName; }
-    set varName(value) { this._varName = value; }
-    get varValue() { return this.main.detail.itemTemplate.extended.getCSS_localVar(this.varName); }
-    set varValue(val) { console.log('assigned'); this.main.detail.itemTemplate.extended.setCSS_localVar(this.varName, val); }
-    getPrevIndex(index) {
-        let rm = this.measurement;
-        index--;
-        for (; index >= 0 && rm[index].size == 0; index--);
-        return index;
-    }
-    get measureText() {
-        return this.measurement.length <= 1 ?
-            'auto'
-            :
-            this.fillSize ?
-                this.measurement
-                    .map(s => s.size)
-                    .slice(0, -1)
-                    .join('px ') + 'px auto'
-                :
-                this.measurement
-                    .map(s => s.size)
-                    .join('px ') + 'px';
+      },
+      onUp: (e, diff) => {
+        if (this.isResizing) this.updateAr();
+        isSettingSize = false;
+        this.collissionResult.hasCollied = false;
+        this.collissionResult.index = -1;
+        this.isResizing = false;
+      },
+    });
 
-    }
+    this.options.grid.addEventListener("mouseenter", (e) => {
+      this.updateOffset();
+      this.options.container.addEventListener(
+        "mousemove",
+        this.mousemovelistner
+      );
+      this.hasMouseEntered = true;
+    });
+    this.options.grid.addEventListener("mouseleave", (e) => {
+      this.hasMouseEntered = false;
+      this.options.container.removeEventListener(
+        "mousemove",
+        this.mousemovelistner
+      );
+    });
+  }
+  Events = {
+    /**
+     * @type {{on:(callback = (
+     *          evt:MouseEvent,
+     *          diff:Point,
+     * ) =>{})} & commonEvent}
+     */
+    onResizing: new commonEvent(),
+  };
 
+  parentOffset = new DOMRect();
+  updateOffset() {
+    this.parentOffset = this.options.grid.getClientRects()[0];
+    this.parentOffset.x -= this.options.grid.scrollLeft;
+    this.parentOffset.y -= this.options.grid.scrollTop;
+  }
+  hasMouseEntered = false;
+  isCheckingHoverCollission = false;
+  collissionResult = { hasCollied: false, index: -1 };
+  /** @param {MouseEvent} __e  */
+  mousemovelistner = (__e) => {
+    if (this.isResizing || this.isCheckingHoverCollission) return;
+    let ete = __e;
+    this.isCheckingHoverCollission = true;
+    setTimeout(() => {
+      if (!this.hasMouseEntered) return;
+      this.isCheckingHoverCollission = false;
+      let x = ete.clientX - this.parentOffset.x;
+      this.collissionResult = this.hasCollission(x);
+      this.options.container.style.cursor = this.collissionResult.hasCollied
+        ? "e-resize"
+        : "";
+    }, 1);
+  };
+  _varName = "gridsize";
+  get varName() {
+    return this._varName;
+  }
+  set varName(value) {
+    this._varName = value;
+  }
+
+  getPrevIndex(index) {
+    let rm = this.measurement;
+    index--;
+    for (; index >= 0 && rm[index].size == 0; index--);
+    return index;
+  }
+  get measureText() {
+    return this.measurement.length <= 1
+      ? "auto"
+      : this.fillSize
+      ? this.measurement
+          .map((s) => s.size)
+          .slice(0, -1)
+          .join("px ") + "px auto"
+      : this.measurement.map((s) => s.size).join("px ") + "px";
+  }
 }
 module.exports = { resizeManage };
