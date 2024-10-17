9514be8612444a12ca111e053303097d95db0157
pratik2201 <pratik2201.pp@gmail.com>
2024-10-17 22:55:23 +0530

    f

--- main.ts
+++ main.ts
@@ -1,15 +1,8 @@
 //console.log('~~~:['+ __dirname +']');
 import path from "path";
 import ucb from "ucbuilder/register"; 
+import "ucbuilder/extension";
 ucb.registar({
-    //srcDir: __dirname,
     outDir: "/out/",
-    rootDir: path.dirname(__dirname),
-    /*html: __dirname,
-    style: __dirname,
-    perameters: __dirname,
-    designer:__dirname,
-    designerSrc:__dirname,
-    code: __dirname,
-    codeSrc: __dirname,*/
+    rootDir: path.dirname(__dirname)
 });
