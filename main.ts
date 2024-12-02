f0fbdc81f9fdbc957ff8535576aa06f66622b35f
pratik2201 <pratik2201.pp@gmail.com>
2024-12-03 03:49:45 +0530

    h

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
-import path from "path";
-import ucr from "ucbuilder";
-import 'module-alias/register';
+//import path from "path";
+//import ucr from "ucbuilder/register";
+import s from "ucbuilder";
+let cfg = s(__dirname);
+
 import "ucbuilder/extension";
 
-ucr.registar({
+/*ucr.registar({
     outDir: "/out/",    
     designerDir:"/designer/",
     rootDir: path.dirname(__dirname)
-});
+});*/
